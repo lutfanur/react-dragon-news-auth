f7effb7d04de67033d2a102336444b46ce403f71
Lutfaakter <lutfanur20@gmail.com>
2024-04-10 21:17:14 +0100

    initialies new project about dragon news

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBa2mVxr4FFG2i_f7rXDgSNmSNoVVUesnw",
+  authDomain: "react-dragon-news-auth-224ec.firebaseapp.com",
+  projectId: "react-dragon-news-auth-224ec",
+  storageBucket: "react-dragon-news-auth-224ec.appspot.com",
+  messagingSenderId: "840565961872",
+  appId: "1:840565961872:web:6686bdfffb33fcf5d65415"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+           <h2 className="text-red-500 text-2xl text-center">This is my Error page</h2> 
+           <img src="https://i.ibb.co/47cJj4c/errorpage2.png" alt="image"  className="max-w-full mx-auto " />
+        </div>
+    );
+};
+
+export default ErrorPage;
